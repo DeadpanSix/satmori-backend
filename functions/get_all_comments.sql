7bebcb7a94e1a157f1d2c199d025232af1ddb7c8
Gerardo Quintero Solorio <gera.quinteros@hotmail.com>
2025-10-10 19:55:59 -0700

    function corrections

--- functions/get_all_comments.sql
+++ functions/get_all_comments.sql
@@ -1,31 +1,25 @@
+DROP FUNCTION public.get_all_comments();
+
 CREATE OR REPLACE FUNCTION public.get_all_comments()
 RETURNS TABLE(
-    id integer,
     user_name varchar,
     rating integer,
     comment_text varchar,
-    photo_data text,
-    photo_type varchar,
-    approved boolean,
-    created_at timestamp with time zone
+    photo_data text
 )
 LANGUAGE 'plpgsql'
 AS $$
 BEGIN
   RETURN QUERY
   SELECT
-    comments.id,
     comments.user_name,
     comments.rating,
     comments.comment_text,
     CASE
       WHEN comments.photo_data IS NOT NULL THEN
-        'data:' || comments.photo_type || ';base64,' || encode(comments.photo_data::bytea, 'base64')
+        'data:' || comments.photo_type || ';base64,' || comments.photo_data
       ELSE NULL
-    END AS photo_data,
-    comments.photo_type,
-    comments.approved,
-    comments.created_at
+    END AS photo_data
   FROM comments;
 END;
 $$;
